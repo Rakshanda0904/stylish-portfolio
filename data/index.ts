db8083244fdec3ca3793c2fe3e8bf3d388b74d9b
Rakshanda0904 <mridulapatil049@gmail.com>
2025-07-07 23:05:48 +0530

    Initial commit

--- data/index.ts
+++ data/index.ts
@@ -113,25 +113,25 @@ export const testimonials = [
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Rakshanda was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Rakshanda's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Rakshanda is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Rakshanda was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project.Rakshanda's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Rakshanda is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Rakshanda was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Rakshanda's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Rakshanda is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Rakshanda was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Rakshanda's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Rakshanda is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
